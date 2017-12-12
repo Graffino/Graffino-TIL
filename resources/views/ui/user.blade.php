d247bcfc87087a1201590a644400fc558bb2c104
Nick Ciolpan <nick@graffino.com>
2017-12-12 09:39:10 +0200

    Naive User controls panel

---
+++ resources/views/ui/user.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'Profile')
+
+@section('content')
+  <ul class="list -type-simple -type-inline">
+    <li class="list__item"><a class="link" href="{{ url('/') }}">User</a></li>
+    <li class="list__item"><a class="link" href="{{ url('auth/logout')}}">Sign out</a></li>
+    <li class="list__item"><a class="link" href="{{ url('posts/new') }}">Create Post</a></li>
+    <li class="list__item"><a class="link" href="{{ url('developer/edit') }}">Profile</a></li>
+  </ul>
+@endsection
