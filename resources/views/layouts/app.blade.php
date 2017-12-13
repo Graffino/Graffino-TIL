5a00ee3608c25eb2b53de3f1296718d763b0b016
Paul Jugarean <paul@graffino.com>
2017-12-13 18:20:24 +0200

    Add components to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Today I Learned - @yield('title')</title>
-
-    <style media="screen">
+    <link rel="stylesheet" href="css/app.css">
+    <!-- <style media="screen">
       body { margin: 0; }
       html, body, .app { height: 100%; }
       .app {
@@ -31,27 +31,69 @@
         grid-row: 2 / 5;
         background: green;
       }
-    </style>
+    </style> -->
   </head>
   <body>
     <div class="app">
+      <div style="display: none;">
+        @include('layouts.svgs')
+      </div>
       @include('ui.user')
       @include('ui.flash')
       <header class="hero">
         <h1 class="hero__title">Today I Learned</h1>
-        <div class="hero__subtitle">Follow us on Twitter</div>
+        <a class="hero__subtitle" href="https://twitter.com/graffino">
+          <span>
+            <svg class="icon -size-small -twitter">
+              <use xlink:href="#sprite-twitter"/>
+            </svg>
+            Follow us on Twitter
+          </span>
+        </a>
       </header>
       <main class="main">
         @yield('content')
       </main>
       <aside class="sidebar">
+        <nav class="nav">
           <ul>
-            <li>Search</li>
-            <li>About</li>
-            <li>Statistics</li>
-            <li>Twitter</li>
-            <li>Random</li>
+            <li>
+              <a class="link" href="#">
+                <svg class="icon -size-medium -magnifier">
+                  <use xlink:href="#sprite-magnifier"/>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a class="link" href="#">
+                <svg class="icon -size-medium -question-mark">
+                  <use xlink:href="#sprite-question-mark"/>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a class="link" href="#">
+                <svg class="icon -size-medium -line-chart">
+                  <use xlink:href="#sprite-line-chart"/>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a class="link" href="#">
+                <svg class="icon -size-medium -twitter">
+                  <use xlink:href="#sprite-twitter"/>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a class="link" href="#">
+                <svg class="icon -size-medium -dice">
+                  <use xlink:href="#sprite-dice"/>
+                </svg>
+              </a>
+            </li>
           </ul>
+        </nav>
       </aside>
     </div>
   </body>
