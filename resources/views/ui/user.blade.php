13469559024ff485f2b87e7994e52c5c256e3cf9
Paul Jugarean <paul@graffino.com>
2017-12-15 16:46:20 +0200

    Add styling to user control list

--- resources/views/ui/user.blade.php
+++ resources/views/ui/user.blade.php
@@ -1,8 +1,10 @@
 @if(Auth::check())
-  <ul class="list -type-simple -type-inline">
+<div class="user__controls">
+  <ul class="controls__list">
     <li class="list__item"><a class="link" href="{{ url('/') }}">User</a></li>
     <li class="list__item"><a class="link" href="{{ url('auth/logout')}}">Sign out</a></li>
     <li class="list__item"><a class="link" href="{{ url('posts/new') }}">Create Post</a></li>
     <li class="list__item"><a class="link" href="{{ url('developer/edit') }}">Profile</a></li>
   </ul>
+</div>
 @endif
