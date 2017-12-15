6f28a07b5716a6e3694b524470cafa28a05cc00d
Paul Jugarean <paul@graffino.com>
2017-12-15 17:42:30 +0200

    Add Jeet and grid

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
       @include('ui.user')
       @include('ui.flash')
       <header class="hero">
-        <div class="hero__container">
+        <div class="container">
           <h1 class="hero__title">Today I Learned</h1>
           <div class="hero__links">
             <a class="hero__subtitle" href="https://twitter.com/graffino">
