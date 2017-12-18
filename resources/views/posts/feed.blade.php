5a7db92589165f218e86ba6ba6cbc15de5205f09
Paul Jugarean <paul@graffino.com>
2017-12-18 12:59:21 +0200

    Add post menu

--- resources/views/posts/feed.blade.php
+++ resources/views/posts/feed.blade.php
@@ -6,7 +6,7 @@
   <div class="feed__wrapper">
     @foreach($posts as $post)
       <div class="post">
-        <div class="container">
+        <div class="container -no-padding">
           <div class="post__wrapper">
             <h1 class="post__title">{{ $post->title }}</h1>
             <p class="post__content">{{ $post->body }}</p>
@@ -14,10 +14,20 @@
               <span class="post__author">{{ $post->developer->username }}</span>
               <span class="post__timestamp">{{ $post->created_at }}</span>
             </div>
-            <ul>
-              <li><a href="#">{{ $post->channel->name }}</a></li>
-            </ul>
-          </div>  
+          </div>
+          <ul class="container--fluid">
+            <li><a class="post__tag link" href="#">{{ $post->channel->name }}</a></li>
+            <li><a class="post__permalink link" href="/posts/{{ $post->id }}">Permalink</a></li>
+            <li><a class="post__raw link" href="#">Raw</a></li>
+            <li>
+              <a class="post__like link" href="#">
+                <svg class="icon -size-xsmall -heart">
+                  <use xlink:href="#sprite-heart"/>
+                </svg>
+                Like
+              </a>
+            </li>
+          </ul>
         </div>
       </div>
     @endforeach
