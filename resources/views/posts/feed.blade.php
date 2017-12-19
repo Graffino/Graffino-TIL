64de3b882b6e5762b1cfc7d1db53b76af3e1e363
Paul Jugarean <paul@graffino.com>
2017-12-19 16:44:06 +0200

    Change container

--- resources/views/posts/feed.blade.php
+++ resources/views/posts/feed.blade.php
@@ -3,33 +3,33 @@
 @section('title', 'Profile')
 
 @section('content')
+<div class="container">
   <div class="feed__wrapper">
     @foreach($posts as $post)
       <div class="post">
-        <div class="container -no-padding">
-          <div class="post__wrapper">
-            <h1 class="post__title">{{ $post->title }}</h1>
-            <p class="post__content">{{ $post->body }}</p>
-            <div class="post__info">
-              <span class="post__author">{{ $post->developer->username }}</span>
-              <span class="post__timestamp">{{ $post->created_at }}</span>
-            </div>
+        <div class="post__wrapper">
+          <h1 class="post__title">{{ $post->title }}</h1>
+          <p class="post__content">{{ $post->body }}</p>
+          <div class="post__info">
+            <span class="post__author">{{ $post->developer->username }}</span>
+            <span class="post__timestamp">{{ $post->created_at }}</span>
           </div>
-          <ul class="container--fluid">
-            <li><a class="post__tag link" href="#">{{ $post->channel->name }}</a></li>
-            <li><a class="post__permalink link" href="/posts/{{ $post->id }}">Permalink</a></li>
-            <li><a class="post__raw link" href="#">Raw</a></li>
-            <li>
-              <a class="post__like link" href="#">
-                <svg class="icon -size-xsmall -heart">
-                  <use xlink:href="#sprite-heart"/>
-                </svg>
-                Like
-              </a>
-            </li>
-          </ul>
         </div>
+        <ul class="container--fluid">
+          <li><a class="post__tag link" href="#">{{ $post->channel->name }}</a></li>
+          <li><a class="post__permalink link" href="/posts/{{ $post->id }}">Permalink</a></li>
+          <li><a class="post__raw link" href="#">Raw</a></li>
+          <li>
+            <a class="post__like link" href="#">
+              <svg class="icon -size-xsmall -heart">
+                <use xlink:href="#sprite-heart"/>
+              </svg>
+              Like
+            </a>
+          </li>
+        </ul>
       </div>
     @endforeach
   </div>
+</div>
 @endsection
