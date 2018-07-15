54f07807bcc03359509a1d2c1f89a74e4e59694d
Nick Ciolpan <nick@graffino.com>
2018-07-15 23:13:04 +0300

    Corrected editor source check

--- resources/assets/js/editor-handler.js
+++ resources/assets/js/editor-handler.js
@@ -3,41 +3,41 @@ let choiceSource = document.getElementById('editor-choice');
 
 if (choiceSource != undefined) {
   choice = choiceSource.dataset.choice;
-}
 
-if (/Code Editor|Vim|Emacs/.test(choice)) {
-  let options;
+  if (/Code Editor|Vim|Emacs/.test(choice)) {
+    let options;
 
-  const defaultOptions = {
-    lineNumbers: true,
-    tabSize: 2,
-    mode: 'gfm',
-    insertSoftTab: true,
-    smartIndent: false,
-    lineWrapping: true
-  };
+    const defaultOptions = {
+      lineNumbers: true,
+      tabSize: 2,
+      mode: 'gfm',
+      insertSoftTab: true,
+      smartIndent: false,
+      lineWrapping: true
+    };
 
-  switch (choice) {
-    case 'Vim':
-      options = Object.assign({}, defaultOptions, {keyMap: 'vim'});
-    break;
-    case 'Emacs':
-      options = Object.assign({}, defaultOptions, {keyMap: 'emacs'});
-    break;
-    default:
-      options = defaultOptions;
-  }
+    switch (choice) {
+      case 'Vim':
+        options = Object.assign({}, defaultOptions, {keyMap: 'vim'});
+      break;
+      case 'Emacs':
+        options = Object.assign({}, defaultOptions, {keyMap: 'emacs'});
+      break;
+      default:
+        options = defaultOptions;
+    }
 
-  const codeEditor = CodeMirror.fromTextArea(editor, options);
+    const codeEditor = CodeMirror.fromTextArea(editor, options);
 
-  codeEditor.on('changes', instance => {
-    const value = instance.getValue();
-    editor.value = value;
-    var event = new Event('input', {
-        'bubbles': true,
-        'cancelable': true
+    codeEditor.on('changes', instance => {
+      const value = instance.getValue();
+      editor.value = value;
+      var event = new Event('input', {
+          'bubbles': true,
+          'cancelable': true
+      });
+
+      editor.dispatchEvent(event);
     });
-
-    editor.dispatchEvent(event);
-  });
+  }
 }
