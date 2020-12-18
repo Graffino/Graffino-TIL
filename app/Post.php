66a5e14bd33cf9242529292e760a5806b55e6338
Nick Ciolpan <nick@graffino.com>
2020-12-18 12:17:41 +0200

    Addded record seeds

--- app/Post.php
+++ app/Post.php
@@ -16,22 +16,27 @@ class Post extends Model
     public static $bodyMaxWords = 200;
     public static $titleMaxChars = 50;
 
-    public function routeNotificationForSlack() {
+    public function routeNotificationForSlack()
+    {
       return env('SLACK_POST_ENDPOINT');
     }
 
-    public function developer() {
+    public function developer()
+    {
       return $this->belongsTo('App\Developer');
     }
 
-    public function channel() {
+    public function channel()
+    {
       return $this->belongsTo('App\Channel');
     }
 
-    public static function slugifyTitle($title) {
+    public static function slugifyTitle($title)
+    {
       return str_slug($title, '-');
     }
-    public static function saltSlug($slug) {
+    public static function saltSlug($slug)
+    {
       $salt = substr(preg_replace('/[^A-Za-z0-9\s-]/', '' ,base64_encode(hex2bin(bin2hex(random_bytes(32))))), 0, 10);
       return $salt.'-'.$slug;
     }
