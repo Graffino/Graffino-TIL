1676dccf98c5b1d32a36b58001b6191a508cb447
Ionut Panduru <ionut@graffino.com>
2021-01-05 16:29:27 +0200

    create google login test

--- tests/Feature/GoogleAuthTest.php
+++ tests/Feature/GoogleAuthTest.php
@@ -2,16 +2,13 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Laravel\Socialite\Contracts\Factory as Socialite;
-use PHPUnit\Framework\TestCase;
+use Socialite;
 use Mockery;
-use Laravel\Dusk\Browser;
-use Tests\DuskTestCase;
-
+use Tests\TestCase;
 class GoogleAuthTest extends TestCase
 {
+    use RefreshDatabase;
 /**
      * A basic test example.
      *
@@ -19,34 +16,22 @@ class GoogleAuthTest extends TestCase
      */
     public function testGoogleLogin()
     {
-        $socialiteMock = Mockery::mock(Socialite::class);
         $abstractUser = Mockery::mock('Laravel\Socialite\Two\User');         
-        $abstractUser->shouldReceive('getId') 
-        ->andReturn(1234567890)
-        ->shouldReceive('getEmail')
-        ->andReturn(str_random(10).'@graffino.com')
-        ->shouldReceive('getNickname')
-        ->andReturn('Pseudo')
-        ->shouldReceive('getName')
-        ->andReturn('Arlette Laguiller')
-        ->shouldReceive('getAvatar')
-        ->andReturn('https://en.gravatar.com/userimage');
+        
+        $abstractUser->email = 'test@graffino.com';
+        $abstractUser->name = 'test';
 
         $provider = Mockery::mock('Laravel\Socialite\Contracts\Provider');
 
         $provider->shouldReceive('user')->andReturn($abstractUser);
 
-        $socialiteMock->shouldReceive('driver')->with('google')->andReturn($provider);
+        Socialite::shouldReceive('driver')->with('google')->andReturn($provider);
 
-        // $this->browse(function (Browser $browser) {
-        //     // $browser->visit('/')
-        //             // ->assertSee('Laravel');
-        //     $browser->visit(route("https://til2.graffino.dev/auth/google/callback"))
-        //         ->seePageIs(route("https://til2.graffino.dev/posts/new"));
-        // });
-        
+        $this->get(route('auth.callback', ['provider' => 'google']))
+        ->assertStatus(302)
+        ->assertRedirect(route('posts'));
 
-        // $response->assertStatus(200);
-
+        $this->get(route('posts.new'))
+        ->assertStatus(200);
     }
 }
