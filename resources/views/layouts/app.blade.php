6be8abda50dfec7c9434bcc67ec7625cd3345e2d
Ionut Panduru <ionut@graffino.com>
2021-01-06 13:29:37 +0200

    add wide meta tags

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="domain-name" content="{{ env('APP_URL') }}">
+    <meta name="keywords" content="'graffino til', 'til', 'learning', 'today i learned' , 'things i learned', 'learn', 'programming'">
+    <meta name="description" content="Learn new things everyday, check out our latest tips and tricks!">
 
     <title>@yield('title') - Today I Learned</title>
     <link rel="stylesheet" href="{{ URL::asset('/css/app.css') }}">
