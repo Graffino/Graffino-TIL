2c7f72473f55c9e3c4cec125cd20df8cd270d014
Ionut <ionut@graffino.com>
2021-01-12 11:42:45 +0200

    update rxjs version && update rxjs syntax for makrdown-handler (#16)
    * update rxjs version && update rxjs syntax for makrdown-handler
    
    * update rxjs syntax for like-handler
    
    * fix subscribing while likeSource was empty, not null
    
    * add placeholder for meta tags & label for html preview

--- resources/views/posts/meta.blade.php
+++ resources/views/posts/meta.blade.php
@@ -7,7 +7,7 @@
     <div class="form">
       <label class="form__label">Meta Tags</label>
           <div class="form__label">
-            <input type="text" class="form__input" name="meta_keywords" value="{{ old('$post->seo', $post->seo ?? '') }}">
+            <input type="text" class="form__input" name="meta_keywords" value="{{ old('$post->seo', $post->seo ?? '') }}" placeholder="ex: terminal, linux">
             <label class="form__label">Description</label>
             <textarea id="description" class="form__textarea" name="description">{{ old('$post->description', $post->description ?? '') }}</textarea>
             <label class="form__label">Canonical URL</label>
