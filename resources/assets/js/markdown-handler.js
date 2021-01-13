9185529b914a9978693570b7dc25809fb8e566e3
Ionut Panduru <ionut@graffino.com>
2021-01-13 15:35:42 +0200

    add csrf token to markdown

--- resources/assets/js/markdown-handler.js
+++ resources/assets/js/markdown-handler.js
@@ -5,12 +5,12 @@ import { ajax } from 'rxjs/ajax';
 const markdownSource = document.getElementById('editor');
 const markdownDestination = document.getElementById('html-preview');
 const domain = document.querySelector('meta[name="domain-name"]').getAttribute('content');
+const token = document.head.querySelector('meta[name="csrf-token"]').content;
 
 let markdownSubscription;
 
 const updateHtml = html => markdownDestination.innerHTML = html;
-
-const makeRequest = data => ajax({url:`${domain}/api/convert`, method: 'POST', body: { raw: data }})
+const makeRequest = data => ajax({url:`${domain}/api/convert`, method: 'POST', body: { raw: data }, headers: {"X-CSRF-TOKEN": token}})
   .subscribe(console.log(`Converting "${data}" to html...`));
 
 const $stream = fromEvent(markdownSource, 'input')
