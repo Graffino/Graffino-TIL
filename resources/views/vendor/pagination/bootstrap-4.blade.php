ca57b081828329f375633a1426c870df71c0f9c3
Ionut <ionut@graffino.com>
2021-01-13 13:59:33 +0200

    fix pagination css html & fix api convert route && required params while editing a post (#18)

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,36 +1,13 @@
 @if ($paginator->hasPages())
-    <ul class="pagination">
-        {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <li class="page-item disabled"><span class="page-link">&laquo;</span></li>
-        @else
-            <li class="page-item"><a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">&laquo;</a></li>
-        @endif
-
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <li class="page-item disabled"><span class="page-link">{{ $element }}</span></li>
-            @endif
-
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li class="page-item active"><span class="page-link">{{ $page }}</span></li>
-                    @else
-                        <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
-
+    <ul class="list pagination -type-simple -type-inline">
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li class="page-item"><a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">&raquo;</a></li>
-        @else
-            <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
+            <li class="list__item"><a class="link" href="{{ $paginator->nextPageUrl() }}" rel="next">&lsaquo; Older posts</a></li>
+        @endif
+
+        {{-- Previous Page Link --}}
+        @if (!$paginator->onFirstPage())
+            <li class="list__item"><a class="link" href="{{ $paginator->previousPageUrl() }}" rel="prev">Newer posts &rsaquo;</a></li>
         @endif
     </ul>
 @endif
