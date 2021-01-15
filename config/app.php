000c6a8536e64a1a102a5cfaad1a28ead990367e
Ionut <ionut@graffino.com>
2021-01-15 14:49:00 +0200

    fix-current-url-for-meta-tags (#27)

--- config/app.php
+++ config/app.php
@@ -231,6 +231,7 @@ return [
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'ViewHelper' => app\Helpers\ViewHelper::class,
     ],
 
 ];
