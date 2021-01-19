13a9f24cfecce1cafd8383274ea8240c3b5525b2
Zeno Popovici <zeno@graffino.com>
2021-01-19 16:35:48 +0200

    Update meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,12 +6,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="domain-name" content="{{ env('APP_URL') }}">
     <meta name="keywords" content="@hasSection('meta_keywords')@yield('meta_keywords')@else{{'graffino til, learning, learn, code, programming, webdev, development, sysops'}}@endif">
-    <meta name="description" content="@hasSection('description')@yield('description')@else @yield('title'){{' • TIL - Learn new things everyday! Check out our latest tips and tricks.'}}@endif">
+    <meta name="description" content="@hasSection('description')@yield('description')@else @yield('title'){{' • TIL - Learn new things everyday! Check out our latest tips and tricks we're discovering in our everyday work.'}}@endif">
     <link rel="canonical" href="@hasSection('canonical_url')@yield('canonical_url')@else{{ViewHelper::getCurrentUrl()}}@endif">
     <meta property="og:title" content="@yield('title'){{' • Today I Learned'}}">
     <meta property="og:url" content="{{ViewHelper::getCurrentUrl()}}">
     <meta property="og:type" content="website">
-    <meta property="og:description" content="@hasSection('description')@yield('description')@else @yield('title'){{' • TIL - Learn new things everyday! Check out our latest tips and tricks.'}}@endif">
+    <meta property="og:description" content="@hasSection('description')@yield('description')@else @yield('title'){{' • TIL - Learn new things everyday! Check out our latest tips and tricks we're discovering in our everyday work.'}}@endif">
     <meta property="og:image" content="@hasSection('social_image_url')@yield('social_image_url')@else{{asset('/images/social.jpg')}}@endif">
 
     <title>@yield('title'){{' • Today I Learned'}}</title>
