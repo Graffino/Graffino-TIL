ecdfc49d836f4e46e1315d6361ba9303a453eadb
Zeno Popovici <zeno@graffino.com>
2021-01-30 00:54:29 +0200

    Revert

---
+++ app/Helpers/ApplicationHelper.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Helpers;
+
+class ApplicationHelper
+{
+  static public function canonicalUrl($slug) {
+    $appUrl = env("APP_URL");
+
+    return $appUrl.'/'.$slug;
+  }
+}
