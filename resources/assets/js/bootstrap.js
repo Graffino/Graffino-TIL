27c96be0c3e9f3fc00de87f90f19a32a2b591bfb
Ionut Panduru <ionut@graffino.com>
2021-07-16 12:00:57 +0300

    fix: deprecated method

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,23 +1,25 @@
-window.Rx = require('rxjs');
-window.hljs = require('highlight.js');
-window.Cookies = require('js-cookie');
+window.Rx = require("rxjs");
+window.hljs = require("highlight.js");
+window.Cookies = require("js-cookie");
 
-window.CodeMirror = require('codemirror/lib/codemirror');
-require('codemirror/keymap/vim');
-require('codemirror/keymap/emacs');
+window.CodeMirror = require("codemirror/lib/codemirror");
+require("codemirror/keymap/vim");
+require("codemirror/keymap/emacs");
 
-hljs.initHighlightingOnLoad();
+hljs.highlightAll();
 
-import Echo from 'laravel-echo'
+import Echo from "laravel-echo";
 
-const domainName = document.head.querySelector('meta[name="domain-name"]').content;
+const domainName = document.head.querySelector(
+    'meta[name="domain-name"]'
+).content;
 
-window.Pusher = require('pusher-js');
+window.Pusher = require("pusher-js");
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: '48c555271ceabb01ea19',
-    cluster: 'eu',
+    broadcaster: "pusher",
+    key: "48c555271ceabb01ea19",
+    cluster: "eu",
     encrypted: true,
-    authEndpoint: `${domainName}/broadcasting/auth`
+    authEndpoint: `${domainName}/broadcasting/auth`,
 });
